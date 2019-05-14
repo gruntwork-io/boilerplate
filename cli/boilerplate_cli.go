dcf52b5e0cabbec713d88e10a9a77ed332c99800
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-05-14 12:02:01 -0700

    Use gruntwork-cli functions to enhance help text

--- cli/boilerplate_cli.go
+++ cli/boilerplate_cli.go
@@ -1,94 +1,85 @@
 package cli
 
 import (
-	"github.com/urfave/cli"
 	"fmt"
+
+	"github.com/gruntwork-io/gruntwork-cli/entrypoint"
+	"github.com/urfave/cli"
+
+	"github.com/gruntwork-io/boilerplate/options"
 	"github.com/gruntwork-io/boilerplate/templates"
 	"github.com/gruntwork-io/boilerplate/variables"
-	"github.com/gruntwork-io/boilerplate/options"
 )
 
-// Customize the --help text for the app so we don't show extraneous info
-const CUSTOM_HELP_TEXT = `NAME:
-   {{.Name}} - {{.Usage}}
+const CUSTOM_HELP_TEXT = `Usage: {{.UsageText}}
 
-USAGE:
-   {{if .UsageText}}{{.UsageText}}{{else}}{{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}{{if .Commands}} command [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}{{end}}
-   {{if .VisibleFlags}}
-OPTIONS:
+A tool for generating files and folders (\"boilerplate\") from a set of templates. Examples:
+
+Generate a project in ~/output from the templates in ~/templates:
+
+    boilerplate --template-folder ~/templates --output-folder ~/output
+
+Generate a project in ~/output from the templates in ~/templates, using variables passed in via the command line:
+
+    boilerplate --template-folder ~/templates --output-folder ~/output --var "Title=Boilerplate" --var "ShowLogo=false"
+
+Generate a project in ~/output from the templates in ~/templates, using variables read from a file:
+
+    boilerplate --template-folder ~/templates --output-folder ~/output --var-file vars.yml
+
+
+Options:
+
    {{range .VisibleFlags}}{{.}}
-   {{end}}{{end}}{{if len .Authors}}
-EXAMPLES:
-   Generate a project in ~/output from the templates in ~/templates:
-
-       boilerplate --template-folder ~/templates --output-folder ~/output
-
-   Generate a project in ~/output from the templates in ~/templates, using variables passed in via the command line:
-
-       boilerplate --template-folder ~/templates --output-folder ~/output --var "Title=Boilerplate" --var "ShowLogo=false"
-
-   Generate a project in ~/output from the templates in ~/templates, using variables read from a file:
-
-       boilerplate --template-folder ~/templates --output-folder ~/output --var-file vars.yml
-
-   {{if .Version}}{{if not .HideVersion}}
-VERSION:
-   {{.Version}}
-   {{end}}{{end}}
-AUTHOR(S):
-   {{range .Authors}}{{.}}{{end}}
-   {{end}}{{if .Copyright}}
-COPYRIGHT:
-   {{.Copyright}}
-   {{end}}
-`
+   {{end}}`
 
 func CreateBoilerplateCli(version string) *cli.App {
+	cli.HelpPrinter = entrypoint.WrappedHelpPrinter
 	cli.AppHelpTemplate = CUSTOM_HELP_TEXT
 	app := cli.NewApp()
+	entrypoint.HelpTextLineWidth = 120
 
 	app.Name = "boilerplate"
 	app.Author = "Gruntwork <www.gruntwork.io>"
-	app.Usage = "A tool for generating files and folders (\"boilerplate\") from a set of templates"
 	app.UsageText = "boilerplate [OPTIONS]"
 	app.Version = version
 	app.Action = runApp
 
-	app.Flags = []cli.Flag {
+	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name: options.OPT_TEMPLATE_FOLDER,
+			Name:  options.OPT_TEMPLATE_FOLDER,
 			Usage: "Generate the project from the templates in `FOLDER`.",
 		},
 		cli.StringFlag{
-			Name: options.OPT_OUTPUT_FOLDER,
+			Name:  options.OPT_OUTPUT_FOLDER,
 			Usage: "Create the output files and folders in `FOLDER`.",
 		},
 		cli.BoolFlag{
-			Name: options.OPT_NON_INTERACTIVE,
+			Name:  options.OPT_NON_INTERACTIVE,
 			Usage: fmt.Sprintf("Do not prompt for input variables. All variables must be set via --%s and --%s options instead.", options.OPT_VAR, options.OPT_VAR_FILE),
 		},
 		cli.StringSliceFlag{
-			Name: options.OPT_VAR,
+			Name:  options.OPT_VAR,
 			Usage: "Use `NAME=VALUE` to set variable NAME to VALUE. May be specified more than once.",
 		},
 		cli.StringSliceFlag{
-			Name: options.OPT_VAR_FILE,
+			Name:  options.OPT_VAR_FILE,
 			Usage: "Load variable values from the YAML file `FILE`. May be specified more than once.",
 		},
 		cli.StringFlag{
-			Name: options.OPT_MISSING_KEY_ACTION,
+			Name:  options.OPT_MISSING_KEY_ACTION,
 			Usage: fmt.Sprintf("What `ACTION` to take if a template looks up a variable that is not defined. Must be one of: %s. Default: %s.", options.ALL_MISSING_KEY_ACTIONS, options.DEFAULT_MISSING_KEY_ACTION),
 		},
 		cli.StringFlag{
-			Name: options.OPT_MISSING_CONFIG_ACTION,
+			Name:  options.OPT_MISSING_CONFIG_ACTION,
 			Usage: fmt.Sprintf("What `ACTION` to take if a the template folder does not contain a boilerplate.yml file. Must be one of: %s. Default: %s.", options.ALL_MISSING_CONFIG_ACTIONS, options.DEFAULT_MISSING_CONFIG_ACTION),
 		},
 		cli.BoolFlag{
-			Name: options.OPT_DISABLE_HOOKS,
+			Name:  options.OPT_DISABLE_HOOKS,
 			Usage: "If this flag is set, no hooks will execute.",
 		},
 		cli.BoolFlag{
-			Name: options.OPT_DISABLE_SHELL,
+			Name:  options.OPT_DISABLE_SHELL,
 			Usage: "If this flag is set, no shell helpers will execute. They will instead return the text 'replace-me'.",
 		},
 	}
--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"os"
+	"github.com/gruntwork-io/gruntwork-cli/entrypoint"
+
 	"github.com/gruntwork-io/boilerplate/cli"
-	"github.com/gruntwork-io/boilerplate/util"
-	"github.com/gruntwork-io/boilerplate/errors"
 )
 
 // This variable is set at build time using -ldflags parameters. For more info, see:
@@ -14,19 +13,5 @@ var VERSION string
 // The main entrypoint for boilerplate
 func main() {
 	app := cli.CreateBoilerplateCli(VERSION)
-	err := app.Run(os.Args)
-
-	if err != nil {
-		printError(err)
-		os.Exit(1)
-	}
-}
-
-// Display the given error in the console
-func printError(err error) {
-	if os.Getenv("BOILERPLATE_DEBUG") != "" {
-		util.Logger.Println(errors.PrintErrorWithStackTrace(err))
-	} else {
-		util.Logger.Println(err)
-	}
+	entrypoint.RunApp(app)
 }
