b15679dea261627e1d13fd89bc39a0ce0b837f44
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2020-11-14 03:07:50 -0600

    Test infra does not support nested example dirs

--- integration-tests/test_helpers_unix.go
+++ integration-tests/test_helpers_unix.go
@@ -3,18 +3,28 @@
 package integration_tests
 
 import (
+	"strings"
 	"testing"
 
+	"github.com/gruntwork-io/terratest/modules/files"
 	"github.com/gruntwork-io/terratest/modules/shell"
+	"github.com/stretchr/testify/require"
 )
 
 // Diffing two directories to ensure they have the exact same files, contents, etc and showing exactly what's different
 // takes a lot of code. Why waste time on that when this functionality is already nicely implemented in the Unix/Linux
 // "diff" command? We shell out to that command at test time.
 func assertDirectoriesEqual(t *testing.T, folderWithExpectedContents string, folderWithActualContents string) {
+	// Copy the folder contents to a temp dir for testing purposes without .keep-dir files, which are used to ensure the
+	// directory exists in git.
+	tmpFolder, err := files.CopyFolderToTemp(folderWithExpectedContents, "boilerplate-assert-direq-", func(path string) bool {
+		return !strings.HasSuffix(path, ".keep-dir")
+	})
+	require.NoError(t, err)
+
 	cmd := shell.Command{
 		Command: "diff",
-		Args:    []string{"-r", "-u", folderWithExpectedContents, folderWithActualContents},
+		Args:    []string{"-r", "-u", tmpFolder, folderWithActualContents},
 	}
 	shell.RunCommand(t, cmd)
 }
