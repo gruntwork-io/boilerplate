bbe95d9cb655b9a47aa7e74d1ec0d0b3b71b100a
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2022-10-26 06:46:59 -0500

    Attempt to provide more helpful error message when git URL may be misspelled (#121)

---
+++ integration-tests/error_message_test.go
@@ -0,0 +1,35 @@
+package integration_tests
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/gruntwork-io/boilerplate/cli"
+	"github.com/gruntwork-io/boilerplate/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMisspelledTemplateURLErrorMessage(t *testing.T) {
+	t.Parallel()
+
+	templateFolder := "../test-fixtures/regression-test/misspelled-git"
+
+	outputFolder, err := ioutil.TempDir("", "boilerplate-test-output")
+	require.NoError(t, err)
+	defer os.RemoveAll(outputFolder)
+
+	app := cli.CreateBoilerplateCli()
+	args := []string{
+		"boilerplate",
+		"--template-url",
+		templateFolder,
+		"--output-folder",
+		outputFolder,
+		"--non-interactive",
+	}
+	runErr := app.Run(args)
+	assert.Error(t, runErr, errors.PrintErrorWithStackTrace(runErr))
+	assert.Contains(t, runErr.Error(), "Did you misspell the URL")
+}
