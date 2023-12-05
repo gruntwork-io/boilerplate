47ddf8ea0303e20fde349de35d76a334c38270cf
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2023-12-05 17:10:23 +0000

    Support lists of strings in for_each_reference (#146)

---
+++ test-fixtures/examples-expected-output/dependencies-for-each-reference-list-of-strings/live/dev/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-abcd1234"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "example-dev"
+  }
+}
