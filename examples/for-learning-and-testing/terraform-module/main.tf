c92dbc9ed61f72b8ed378f439f0f01e12f48bc86
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2024-02-27 13:58:35 -0500

    Simplify terraform-module

--- examples/for-learning-and-testing/terraform-module/main.tf
+++ examples/for-learning-and-testing/terraform-module/main.tf
@@ -1,5 +1,17 @@
 terraform {
   required_version = "{{ .TerraformVersion }}"
+
+  {{- if .Providers }}
+
+  required_providers {
+    {{ range $provider_source, $provider_version := .Providers -}}
+    {{ $provider_source }} = {
+      source  = "hashicorp/{{ $provider_source }}"
+      version = "{{ $provider_version }}"
+    }
+    {{- end }}
+  }
+  {{- end }}
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
