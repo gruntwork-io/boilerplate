6e515db5bae2d673e32e8c087d4a2c25d27490fd
Denis O <denis.o@linux.com>
2024-03-05 11:13:21 +0000

    Add terragrunt

--- .circleci/install-terragrunt.ps1
+++ .circleci/install-terragrunt.ps1
@@ -1,5 +1,5 @@
 # Install Terragrunt using Chocolatey
-choco install terraform --version 0.55.10 -y
+choco install terragrunt --version 0.55.10 -y
 # Verify installation
 Get-Command terragrunt
 terragrunt version
