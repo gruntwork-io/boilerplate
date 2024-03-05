1a76df049e0f5619f5bbc50237a628967d2a5fd6
Denis O <denis.o@linux.com>
2024-03-05 11:11:41 +0000

    Add step to install windows packages

---
+++ .circleci/install-terraform.ps1
@@ -0,0 +1,5 @@
+# Install terraform using Chocolatey
+choco install terraform --version 1.5.0 -y
+# Verify installation
+Get-Command terraform
+terraform version
---
+++ .circleci/install-terragrunt.ps1
@@ -0,0 +1,5 @@
+# Install Terragrunt using Chocolatey
+choco install terraform --version 0.55.10 -y
+# Verify installation
+Get-Command terragrunt
+terragrunt version
