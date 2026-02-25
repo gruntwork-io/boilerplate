2dff3078f1173692217db2796b6583e98734d00c
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2026-02-25 10:04:11 -0500

    chore: Exporting `validation` (#287)

--- variables/validation.go
+++
@@ -1,7 +0,0 @@
-package variables
-
-import "github.com/gruntwork-io/boilerplate/internal/validation"
-
-// CustomValidationRule is a type alias re-exported from internal/validation
-// so that external consumers can reference the type by name.
-type CustomValidationRule = validation.CustomValidationRule
